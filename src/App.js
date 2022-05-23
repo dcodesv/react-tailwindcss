29789038bf6e03063104741bc26c922af4f4f900
Diego Armando Villalobos Villalta <39570306+dcodesv@users.noreply.github.com>
2022-05-23 15:42:10 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:mb-8">
+            <span className="block">Ready to use TailwindCSS?</span>
+            <span className="block text-indigo-600">Get started now.</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0 justify-center">
+            <div className="inline-flex rounded-md shadow">
+              <a href="https://tailwindcss.com/docs/installation" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
+            </div>
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a href="https://tailwindcss.com/" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"> Learn more </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    './src/**/*.{html,js,jsx,css,scss}',
+    './src/*.{html,js,jsx,css,scss}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
